7bf04b4e208266106ac0047479108edce05a6635
Stefan Jemuovic <stefan.jemuovic@plusplus.rs>
2022-06-18 23:50:27 +0200

    first commit

---
+++ src/main/java/com/helbiz/HelbizApplication.java
@@ -0,0 +1,13 @@
+package com.helbiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelbizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelbizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helbiz/HelbizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helbiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelbizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
